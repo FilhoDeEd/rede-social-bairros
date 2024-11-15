99cc79a56d9ac957763826f9f3a47b03410f9b6e
FilhoDeEd <edson.er.rodrigues@gmail.com>
2024-11-14 22:19:41 -0300

    Projects Vue and Django

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1', include('djoser.urls')),
-    path('api/v1', include('djoser.urls.authtoken'))
+    path('', include('accounts.urls'))
 ]
