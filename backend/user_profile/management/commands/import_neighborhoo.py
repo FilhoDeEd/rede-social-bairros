85ca5f7aa65d9ce0ca4a8505d369e602baec4cf0
RodolfoHRE <rodolfohre@gmail.com>
2024-11-20 11:52:18 -0300

    Modificações no user_profile
    modificação do models para usar a sigla do estado na classe neighborhood, além de ajustes no import_neighborhood e no gitignore (perdao, tinha esquecido de alterar isso)

--- backend/user_profile/management/commands/import_neighborhoo.py
+++ backend/user_profile/management/commands/import_neighborhoo.py
@@ -2,7 +2,7 @@ import os
 from django.conf import settings
 from django.core.management.base import BaseCommand
 import pandas as pd
-from user_profile.models import Neighborhood  # Ajuste para o nome real do seu modelo
+from user_profile.models import Neighborhood
 
 class Command(BaseCommand):
     help = 'Importa dados de um arquivo Excel para o banco de dados'
@@ -16,17 +16,20 @@ class Command(BaseCommand):
             dados = pd.read_excel(arquivo_excel)
 
             # Validar as colunas necessárias
-            colunas_necessarias = {'Bairro', 'Cidade', 'Estado'}
+            colunas_necessarias = {'id', 'bairro', 'cidade', 'estado'}
             if not colunas_necessarias.issubset(dados.columns):
                 raise ValueError(f'O arquivo Excel deve conter as colunas: {colunas_necessarias}')
 
             # Itera sobre as linhas e cria os objetos no banco
             for _, linha in dados.iterrows():
-                Neighborhood.objects.create(
-                    bairro=linha['Bairro'],
-                    cidade=linha['Cidade'],
-                    estado=linha['Estado']
+                # Cria uma instância do modelo Neighborhood
+                neighborhood = Neighborhood(
+                    name=linha['bairro'],       # Mapeia 'bairro' para 'name'
+                    locality=linha['cidade'],   # Mapeia 'cidade' para 'locality'
+                    state=linha['estado'],      # Mapeia 'estado' para 'state'
                 )
+                # Salva no banco, aplicando validações
+                neighborhood.save()
 
             self.stdout.write(self.style.SUCCESS('Dados importados com sucesso!'))
 
--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,17 +1,49 @@
 from django.db import models
+from accounts import models
+
+class UfChoices(models.TextChoices):
+    AC = 'AC', 'Acre'
+    AL = 'AL', 'Alagoas'
+    AP = 'AP', 'Amapá'
+    AM = 'AM', 'Amazonas'
+    BA = 'BA', 'Bahia'
+    CE = 'CE', 'Ceará'
+    DF = 'DF', 'Distrito Federal'
+    ES = 'ES', 'Espírito Santo'
+    GO = 'GO', 'Goiás'
+    MA = 'MA', 'Maranhão'
+    MT = 'MT', 'Mato Grosso'
+    MS = 'MS', 'Mato Grosso do Sul'
+    MG = 'MG', 'Minas Gerais'
+    PA = 'PA', 'Pará'
+    PB = 'PB', 'Paraíba'
+    PR = 'PR', 'Paraná'
+    PE = 'PE', 'Pernambuco'
+    PI = 'PI', 'Piauí'
+    RJ = 'RJ', 'Rio de Janeiro'
+    RN = 'RN', 'Rio Grande do Norte'
+    RS = 'RS', 'Rio Grande do Sul'
+    RO = 'RO', 'Rondônia'
+    RR = 'RR', 'Roraima'
+    SC = 'SC', 'Santa Catarina'
+    SP = 'SP', 'São Paulo'
+    SE = 'SE', 'Sergipe'
+    TO = 'TO', 'Tocantins'
 
 
 class Neighborhood(models.Model):
     name = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
-    locality = models.CharField(max_length=2)
-    country = models.CharField(max_length=255, default='Brasil')
+    state = models.CharField(max_length=2, choices=UfChoices.choices)
+    locality = models.CharField(max_length=255)
+    country = models.CharField(max_length=255, default='Brazil')
 
     def __str__(self):
         return f"{self.name}, {self.locality}, {self.state}"
 
 
 class User_profile(models.Model):
+
+    #checar todos os status possíveis e ver se algum garante "vantagens"
 
     STATUS_CHOICES = [
         # ('active', 'Active'),
@@ -23,26 +55,15 @@ class User_profile(models.Model):
     surname = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     cellphone = models.CharField(max_length=20)
-    #perfil_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
-    birthday = models.DateField(blank=True, null=True)
+
     trust_rate = models.FloatField()
     active = models.BooleanField(default=True)
-    neighborhood = models.ForeignKey(
-        Neighborhood,
-        on_delete=models.SET_NULL,  # Define o comportamento ao excluir o bairro
-        null=True,
-        blank=True,
-        related_name="residents",  # Permite acessar os usuários pelo bairro
+    neighborhood = models.ForeignKey(Neighborhood,)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
 
-    status = models.CharField(
-            max_length=10, 
-            choices=STATUS_CHOICES, 
-            default='active'
-        )
-
-        #FK id_account
-
-    )
-
+    #parte comentada para analise posterior
+    #id_account = models.ForeignKey(accounts)
+    #perfil_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    
     def __str__(self):
         return f"{self.name} {self.surname}"
