fca1fb763acf41c00b15a74b1d7048e038cd6fc0
Marcos <mhmcamp@gmail.com>
2024-11-20 16:11:19 -0300

    Update Register.vue
    Adjusting elements

--- frontend/src/views/auth/Register.vue
+++ frontend/src/views/auth/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto px-4 h-full">
-    <div class="flex content-center items-center justify-center h-full">
+    <div class="flex content-center items-end justify-center h-full">
       <div class="w-full lg:w-6/12 px-4">
         <div
           class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
