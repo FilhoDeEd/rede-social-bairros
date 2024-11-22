84f6a619cba143e2b3501c19e6b6107d4b0645da
Edson Rodrigues da Cruz Filho <edson.er.rodrigues@gmail.com>
2024-11-22 16:31:39 -0300

    account api : register e login feitos

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,7 +1,9 @@
-from account.serializers import AccountSerializer
+from typing import Dict
+from account.serializers import AccountSerializer, UserSerializer
 
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.db import transaction, IntegrityError
 
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -9,36 +11,61 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-# create account
-    # name surname
-    # birthday
-    # username
-    # email
-    # password
-    # agree_policy
+def add_errors(errors: Dict, serializer_errors: Dict):
+    for field, error in serializer_errors.items():
+        if field in errors:
+            errors[field].extend(error)
+        else:
+            errors[field] = error
 
-    # checar se username é unique
-
-    # checar se email é unique
-
-    # Criar objeto Account e objeto Profile
-        # Popular Account com as informações recebidas
 
 class AccountRegisterView(APIView):
+    """
+    Handles user create account.
+    """
     def post(self, request):
         data = request.data
-        user_data = request.data.get('user')
-        serializer = AccountSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            user = User.objects.get(username=user_data['username'])
-            user.set_password(user_data['password'])
-            user.save()
-            token = Token.objects.create(user=user)
-            return Response({'token': token.key, 'account': serializer.data})
+        user_data = {
+            'username': data.pop('username', ''),
+            'password': data.pop('password', ''),
+        }
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        profile_data = {
+            'state': data.pop('state', ''),
+            'city': data.pop('city', ''),
+            'neighborhood': data.pop('neighborhood', ''),
+        }
+
+        user_serializer = UserSerializer(data=user_data)
+        account_serializer = AccountSerializer(data=data)
+        # profile_serializer = ProfileSerializer(data=profile_data)
+
+        errors = {}
+
+        # Validar cada serializer e acumular erros
+        if not user_serializer.is_valid():
+            add_errors(errors=errors, serializer_errors=user_serializer.errors)
+
+        if not account_serializer.is_valid():
+            add_errors(errors=errors, serializer_errors=account_serializer.errors)
+        
+        # if not profile_serializer.is_valid():
+        #     add_errors(errors=errors, account_serializer=profile_serializer.errors)
+
+        if errors:
+            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            with transaction.atomic():
+                user = user_serializer.save()
+                account = account_serializer.save(user=user)
+                # profile = profile_serializer.save(account=account)
+                token = Token.objects.create(user=user)
+        except Exception as e:
+            return Response({'detail': 'An unexpected error occurred.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
 
 
 class AccountLoginView(APIView):
@@ -56,10 +83,3 @@ class AccountLoginView(APIView):
             return Response({"token": token.key}, status=status.HTTP_200_OK)
         else:
             return Response({"detail": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
-
-
-# Login
-    # username
-    # password
-
-    # autenticar 
