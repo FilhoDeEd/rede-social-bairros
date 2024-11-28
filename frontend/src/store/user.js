758c0c369ab0bbccbb16f5c97cb86d91c60e80e9
Marcos <mhmcamp@gmail.com>
2024-11-28 20:27:09 -0300

    Update user.js

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -8,11 +8,24 @@ export const useUserStore = defineStore({
     state: () => ({
         user:{
             isAutheticated: false,
-            id: null,
-            name: null,
-            email: null,
             acess: null,
             refresh: null,
+
+            id: null,
+            name: null,
+            surname: null,
+            email: null,
+            username:null,
+            cellphone:null,
+            gender:null,
+            biography:null,
+            birthday:null,
+            status:null,
+            
+            state:null,
+            locality:null,
+            neighborhood:null,
+            neighborhood_id:null,
         }
     }),
     actions:{
@@ -21,10 +34,26 @@ export const useUserStore = defineStore({
 
                 this.user.access = localStorage.getItem('user.access')
                 this.user.refresh = localStorage.getItem('user.refresh')
+
                 this.user.id = localStorage.getItem('user.id')
                 this.user.email = localStorage.getItem('user.email')
                 this.user.name = localStorage.getItem('user.name')
+                this.user.surname= localStorage.getItem('user.surname'),
+                this.user.username= localStorage.getItem('user.username'),
+                this.user.cellphone= localStorage.getItem('user.cellphone'),
+                this.user.gender= localStorage.getItem('user.gender'),
+                this.user.biography= localStorage.getItem('user.biography'),
+                this.user.birthday= localStorage.getItem('user.birthday'),
+                this.user.birthday= localStorage.getItem('user.status'),
+
+                
+                this.user.state= localStorage.getItem('user.state'),
+                this.user.locality= localStorage.getItem('user.locality'),
+                this.user.neighborhood= localStorage.getItem('user.neighborhood'),
+                this.user.neighborhood_id= localStorage.getItem('user.neighborhood_id'),
+
                 this.user.isAutheticated = true
+
 
                 this.refreshToken()
 
@@ -36,10 +65,10 @@ export const useUserStore = defineStore({
             console.log('setToken',data)
 
             this.user.access = data.access
-            this.user.refresh = data.refresh
+            //this.user.refresh = data.refresh
             this.user.isAutheticated = true
             localStorage.setItem('user.access', data.access)
-            localStorage.setItem('user.refresh', data.refresh)
+            //localStorage.setItem('user.refresh', data.refresh)
 
         },
 
@@ -50,24 +79,77 @@ export const useUserStore = defineStore({
             this.user.id = false
             this.user.name = false
             this.user.email = false
+            this.user.surname= false
+            this.user.username= false
+            this.user.cellphone= false
+            this.user.gender= false
+            this.user.biography= false
+            this.user.birthday= false
+            this.user.status= false
+
+            
+            this.user.state= false
+            this.user.locality= false
+            this.user.neighborhood= false
+            this.user.neighborhood_id= false
+
 
             localStorage.setItem('user.access','')
             localStorage.setItem('user.refresh','')
+
             localStorage.setItem('user.id','')
             localStorage.setItem('user.name','')
             localStorage.setItem('user.email','')
+            localStorage.setItem('user.surname','')
+            localStorage.setItem('user.username','')
+            localStorage.setItem('user.cellphone','')
+            localStorage.setItem('user.gender','')
+            localStorage.setItem('user.biography','')
+            localStorage.setItem('user.birthday','')
+            localStorage.setItem('user.status','')
+
+            localStorage.setItem('user.state','')
+            localStorage.setItem('user.locality','')
+            localStorage.setItem('user.neighborhood','')
+            localStorage.setItem('user.neighborhood_id','')
 
         },
 
         setUserInfo(user){
             console.log("setUserInfo",user)
+
             this.user.id = user.id
             this.user.name = user.name
             this.user.email = user.email
+            this.user.surname = user.surname
+            this.user.username = user.username
+            this.user.cellphone = user.cellphone
+            this.user.gender = user.gender
+            this.user.biography = user.biography
+            this.user.birthday = user.birthday
+            this.user.status = user.status
+
+            
+            this.user.state= user.state
+            this.user.locality= user.locality
+            this.user.neighborhood= user.neighborhood
+            this.user.neighborhood_id= user.neighborhood_id
 
             localStorage.setItem('user.id', this.user.id)
             localStorage.setItem('user.name', this.user.name)
             localStorage.setItem('user.email', this.user.email)
+            localStorage.setItem('user.surname', this.user.surname)
+            localStorage.setItem('user.username', this.user.username)
+            localStorage.setItem('user.cellphone', this.user.cellphone)
+            localStorage.setItem('user.gender', this.user.gender)
+            localStorage.setItem('user.biography', this.user.biography)
+            localStorage.setItem('user.birthday', this.user.birthday)
+            localStorage.setItem('user.status', this.user.status)
+
+            localStorage.setItem('user.state', this.user.state)
+            localStorage.setItem('user.locality', this.user.locality)
+            localStorage.setItem('user.neighborhood', this.user.neighborhood)
+            localStorage.setItem('user.neighborhood_id', this.user.neighborhood_id)
 
             console.log('User', this.user)
         },
