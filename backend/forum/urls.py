dbc8cc668ce5c665a670fa4fc14dbb27bfd15d73
RolfE-HR <rodolfohre@gmail.com>
2024-12-03 13:55:10 -0300

    Feito a parte de inscrição em forum

--- backend/forum/urls.py
+++ backend/forum/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from forum.views import (
     ForumRegisterView,
-    ForumListView
+    ForumListView,
+    SubscribeView
 )
 
 
 urlpatterns = [
     path('register/',ForumRegisterView.as_view(), name='forum_register'),
-    path('list/', ForumListView.as_view(), name='forum_list')
+    path('list/', ForumListView.as_view(), name='forum_list'),
+    path('subscribe/<int:forum_id>/', SubscribeView.as_view(), name='forum_subscribe')
 ]
