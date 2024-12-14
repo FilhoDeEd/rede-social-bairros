cf8d301f99519b8462499d6ca54113767fb81d62
Edson Rodrigues <edson.er.rodrigues@gmail.com>
2024-12-14 16:57:12 -0300

    Bug fix: frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,29 +9,29 @@
 </style>
 
 <script>
-import { onBeforeMount } from 'vue';
-import { useUserStore } from './store/user';
-import axios from 'axios';
+  import axios from 'axios';
+  import { useUserStore } from '@/store/user';
+  import { onBeforeMount } from 'vue';
 
-export default {
-  setup() {
-    const userStore = useUserStore();
-    
-    onBeforeMount(() => {
-      userStore.initStore();
+  export default {
+    setup() {
+      const userStore = useUserStore();
+      
+      onBeforeMount(() => {
+        userStore.initStore();
 
-      const token = userStore.user.access;
+        const token = userStore.user.access;
 
-      if (token) {
-        axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-      } else {
-        axios.defaults.headers.common["Authorization"] = "";
-      }
-    });
+        if (token) {
+          axios.defaults.headers.common["Authorization"] = "Token " + token;
+        } else {
+          axios.defaults.headers.common["Authorization"] = "";
+        }
+      });
 
-    return {
-      userStore
-    };
-  }
-};
+      return {
+        userStore
+      };
+    }
+  };
 </script>
